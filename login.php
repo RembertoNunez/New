e30e7bca6168407d9bc09fcdd9353785562ddc06
Remberto <Nunez@Rembertos-MacBook-Pro.local>
2018-06-26 11:13:05 -0600

    first commit

--- login.php
+++ login.php
@@ -23,24 +23,24 @@
       <div class="card-body">
         <form>
           <div class="form-group">
-            <label for="exampleInputEmail1">Email address</label>
+            <label for="exampleInputEmail1">Correo Electronico</label>
             <input class="form-control" id="exampleInputEmail1" type="email" aria-describedby="emailHelp" placeholder="Enter email">
           </div>
           <div class="form-group">
-            <label for="exampleInputPassword1">Password</label>
+            <label for="exampleInputPassword1">Contraseña</label>
             <input class="form-control" id="exampleInputPassword1" type="password" placeholder="Password">
           </div>
           <div class="form-group">
             <div class="form-check">
               <label class="form-check-label">
-                <input class="form-check-input" type="checkbox"> Remember Password</label>
+                <input class="form-check-input" type="checkbox"> Recordar Contraseña</label>
             </div>
           </div>
           <a class="btn btn-primary btn-block" href="index.html">Login</a>
         </form>
         <div class="text-center">
-          <a class="d-block small mt-3" href="register.html">Register an Account</a>
-          <a class="d-block small" href="forgot-password.html">Forgot Password?</a>
+          <a class="d-block small mt-3" href="register.html">Registrar Cuenta</a>
+          <a class="d-block small" href="forgot-password.html">Olvido su Constraseña?</a>
         </div>
       </div>
     </div>
