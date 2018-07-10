9ffc747ee470d03c7dc36539324121e7917e727a
Remberto <Nunez@Rembertos-MacBook-Pro.local>
2018-07-10 16:19:31 -0600

    enable database

--- funciones/getConnection.php
+++ funciones/getConnection.php
@@ -1,17 +1,17 @@
 <?php
 function getDBConnection() {
     //Informacion de base de datos
-    // $host = "us-cdbr-iron-east-04.cleardb.net";
-    // $db = "heroku_91371adea50de1b";
-    // $user = "bb3ac46965aa56";
-    // $pass = "cadb2033";
-    // $charset ="utf8mb4";
-
-    $host = "localhost";
-    $db = "sesc";
-    $user = "root";
-    $pass = "root";
+    $host = "us-cdbr-iron-east-04.cleardb.net";
+    $db = "heroku_91371adea50de1b";
+    $user = "bb3ac46965aa56";
+    $pass = "cadb2033";
     $charset ="utf8mb4";
+
+    // $host = "localhost";
+    // $db = "sesc";
+    // $user = "root";
+    // $pass = "root";
+    // $charset ="utf8mb4";
     
     //Estableciendo Conexion
     $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
