7e134324cb83bb972dbb4c157dd2f6b9b5bde22a
Remberto <Nunez@Rembertos-MacBook-Pro.local>
2018-08-01 09:53:39 -0600

    google analitics

--- standar/standar.php
+++ standar/standar.php
@@ -14,6 +14,15 @@ function standar_footer() {
     </div>
   </div>
 </footer>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script src="https://www.googletagmanager.com/gtag/js?id=UA-121951606-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-121951606-1');
+</script>
 <?php
 }
 ?>
